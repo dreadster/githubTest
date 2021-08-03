117be59681d5a3c7d0b96b7e930e15cc0b424ea5
dreadster <88404415+dreadster@users.noreply.github.com>
2021-08-03 16:22:20 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello, World! Page</title>
+</head>
+<body>
+<?php
+	echo 'Hello, World!';
+	echo '<br>';
+	echo 'Change 1 1507';
+	echo '<br>';
+	echo 'Change 2 1508';
+	echo '<br>';
+	echo 'love Azure!';
+	echo '<br>';
+	echo 'easy';
+?>
+</body>
+</html>
